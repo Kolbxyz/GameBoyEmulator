ab7293876184203a498bcc654f7795f587b8db64
Timothée KREMER <timothee.kremer@epitech.eu>
2026-02-08 23:40:36 +0100

    feat: Implement opcode 0x4C

--- src/main.c
+++ src/main.c
@@ -297,6 +297,7 @@ int main() {
                         case 0x49: cpu.registers.c = cpu.registers.c; INCR(1); // LD C, C
                         case 0x4A: cpu.registers.c = cpu.registers.d; INCR(1); // LD C, D
                         case 0x4B: cpu.registers.c = cpu.registers.e; INCR(1); // LD C, E
+                        case 0x4C: cpu.registers.c = cpu.registers.h; INCR(1); // LD C, H
             // MAIN OPCODES
         }
     }
