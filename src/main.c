e1d0bf275fbb3b2a3feb139e97a5eb20051a87c2
Timothée KREMER <timothee.kremer@epitech.eu>
2026-02-08 23:40:46 +0100

    feat: Implement opcode 0xAC

--- src/main.c
+++ src/main.c
@@ -436,6 +436,7 @@ int main() {
                         case 0xA9: cpu_xor(&cpu, cpu.registers.c); INCR(1);
                         case 0xAA: cpu_xor(&cpu, cpu.registers.d); INCR(1);
                         case 0xAB: cpu_xor(&cpu, cpu.registers.e); INCR(1);
+                        case 0xAC: cpu_xor(&cpu, cpu.registers.h); INCR(1);
             // MAIN OPCODES
         }
     }
