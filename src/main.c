f26d85425f6ba1f84ac0eadd0b994ba98ff296d5
Timothée KREMER <timothee.kremer@epitech.eu>
2026-02-08 23:40:44 +0100

    feat: Implement opcode 0x9D

--- src/main.c
+++ src/main.c
@@ -409,6 +409,7 @@ int main() {
                         case 0x9A: cpu_sbc(&cpu, cpu.registers.d); INCR(1); // SBC A, D
                         case 0x9B: cpu_sbc(&cpu, cpu.registers.e); INCR(1); // SBC A, E
                         case 0x9C: cpu_sbc(&cpu, cpu.registers.h); INCR(1); // SBC A, H
+                        case 0x9D: cpu_sbc(&cpu, cpu.registers.l); INCR(1); // SBC A, L
             // MAIN OPCODES
         }
     }
