bd2c5bc6307f4fb945e25a21611f2ffba0388e35
Timothée KREMER <timothee.kremer@epitech.eu>
2026-02-08 23:40:41 +0100

    feat: Implement opcode 0x7B

--- src/main.c
+++ src/main.c
@@ -356,6 +356,7 @@ int main() {
                         case 0x78: cpu.registers.a = cpu.registers.b; INCR(1); // LD A, B
                         case 0x79: cpu.registers.a = cpu.registers.c; INCR(1); // LD A, C
                         case 0x7A: cpu.registers.a = cpu.registers.d; INCR(1); // LD A, D
+                        case 0x7B: cpu.registers.a = cpu.registers.e; INCR(1); // LD A, E
             // MAIN OPCODES
         }
     }
