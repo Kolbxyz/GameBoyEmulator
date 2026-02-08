bc9adfb121fb294a477908f8e8765134f2e20199
Timothée KREMER <timothee.kremer@epitech.eu>
2026-02-08 23:40:40 +0100

    feat: Implement opcode 0x75

--- src/main.c
+++ src/main.c
@@ -348,6 +348,7 @@ int main() {
                         case 0x72: cpu.memory[cpu.registers.hl] = cpu.registers.d; INCR(1); // LD (HL), D
                         case 0x73: cpu.memory[cpu.registers.hl] = cpu.registers.e; INCR(1); // LD (HL), E
                         case 0x74: cpu.memory[cpu.registers.hl] = cpu.registers.h; INCR(1); // LD (HL), H
+                        case 0x75: cpu.memory[cpu.registers.hl] = cpu.registers.l; INCR(1); // LD (HL), L
             // MAIN OPCODES
         }
     }
